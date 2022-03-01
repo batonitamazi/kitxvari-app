e2345a32f3c230a35b44fd76cb376f8f1a55932a
batonitamazi <tamazi.mirianashvili.1@btu.edu.ge>
2022-03-01 11:41:51 +0400

    added routing and pages

---
+++ kitxvari-app/src/routing.js
@@ -0,0 +1,24 @@
+import React from "react"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+// import CovidPage from './pages/CovidPage'
+import LandingPage from './pages/LandingPage'
+// import PersInfoPage from './pages/PersInfoPage'
+// import rdbrInsightPage from './pages/rdbrInsightPage'
+// import SubmitPage from './pages/SubmitPage'
+// import TechSkillPage from './pages/TechSkillPage'
+// import ThanksPage from './pages/ThanksPage'
+
+
+
+const Routing = ()  => { 
+    return <Switch> 
+        <Route path='/' component={LandingPage} exact/>
+        {/* <Route path='/personalinfo' component={PersInfoPage} exact/>
+        <Route path='/techskill' component={TechSkillPage} exact/>
+        <Route path='/covid' component={CovidPage} exact/>
+        <Route path='/rdbrinsight' component={rdbrInsightPage} exact/>
+        <Route path='/submit' component={SubmitPage} exact/>
+        <Route path='/thanks' component={ThanksPage} exact/> */}
+    </Switch>
+}
+export default Routing
